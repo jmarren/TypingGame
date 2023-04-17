a208f3570d49a69a481ccd60f12eb34425a3187f
John Marren <jmarren99@gmail.com>
2023-04-17 09:58:23 -0700

    Context and Multiple Modals Added

---
+++ src/Components/GameOver.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { AppContext } from '../../AppContext'
+
+
+const GameOver = (props) =>  {
+ const { closeGameOver } = useContext(AppContext)
+    
+
+    const { typedWords } = props
+
+
+
+    return (
+       <div><div style={{
+            fontSize: '50px',
+            zIndex: '100',
+            backgroundColor: 'yellow',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'fixed',
+            top: '25%',
+            left: '30%',
+            height: '40%',
+            width: '40%',
+            borderRadius: '20px'
+        }}><button  onClick={closeGameOver} style={{ position: 'absolute', top: 10, right: 10 }}>X</button>
+            Game Over!<br />Words/Minute: {typedWords} <br />Total Words: {typedWords}<br /> <button style={{ position: 'absolute', bottom: 15, height: '30px' }}>Play Again</button></div>
+    </div>
+         )
+}
+
+export default GameOver;
